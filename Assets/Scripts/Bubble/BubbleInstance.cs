16c918f63fe1d0b744635022fb5720c9cb290d18
Guilherme Siqueira Bianchini <gui.bianchini@gmail.com>
2025-01-25 13:04:47 -0400

    bolha troy double e acid

--- Assets/Scripts/Bubble/BubbleInstance.cs
+++ Assets/Scripts/Bubble/BubbleInstance.cs
@@ -28,30 +28,27 @@ public class BubbleInstance : MonoBehaviour
     {
 
 
-        //  //float newX = Random.Range(1, 2);
-        //  //float newY = Random.Range(1, 2);
+        float newX = Random.Range(0.5f, 1);
+        float newY = Random.Range(0.5f,1 );
 
-        // /* GameObject newBubble1 =*/ Instantiate(bubbles, bubblesSpace);
-        //  //newBubble1.transform.position = position.position;
-        //  //newBubble1.transform.Translate(new Vector3(newX, newY, 0));
-        //  BubbleMovement.Instance.NewBubble();
-        ///*  GameObject newBubble2 = */Instantiate(bubbles, bubblesSpace);
-        //  //newBubble2.transform.position = position.position;
-        //  //newBubble2.transform.Translate(new Vector3(newX, newY, 0));
-        //  BubbleMovement.Instance.NewBubble();
+        GameObject newBubble1 =Instantiate(bubbles, bubblesSpace);
+        newBubble1.transform.position = position.position;
+        newBubble1.transform.Translate(new Vector3(newX, newY, 0));
+        BubbleMovement.Instance.NewBubble();
+         GameObject newBubble2 = Instantiate(bubbles, bubblesSpace);
+        newBubble2.transform.position = position.position;
+        newBubble2.transform.Translate(new Vector3(newX, newY, 0));
+        BubbleMovement.Instance.NewBubble();
+        newBubble1.gameObject.GetComponent<BubbleCollider>().ImunityBubble();
+        newBubble2.gameObject.GetComponent<BubbleCollider>().ImunityBubble();
 
-        StartCoroutine(WaitTime());
+        // StartCoroutine(WaitTime());
     }
 
-    IEnumerator WaitTime()
-    {
-        yield return new WaitForSeconds(Random.Range(1, 4));
-        Instantiate(bubbles, bubblesSpace);
-        BubbleMovement.Instance.NewBubble();
-        yield return new WaitForSeconds(Random.Range(1, 4));
-        Instantiate(bubbles, bubblesSpace);
+    //IEnumerator WaitTime()
+    //{
+    //    yield return new WaitForSeconds(R);
+        
 
-        BubbleMovement.Instance.NewBubble();
-
-    }
+    //}
 }
